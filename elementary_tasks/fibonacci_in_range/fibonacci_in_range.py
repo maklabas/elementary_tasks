1cc09669ea23f13879da6a9ae4c2ddccce0f48da
Pahan <5972140@gmail.com>
2021-09-18 13:43:24 +0300

    second task task has argparser

--- elementary_tasks/fibonacci_in_range/fibonacci_in_range.py
+++ elementary_tasks/fibonacci_in_range/fibonacci_in_range.py
@@ -1,24 +1,38 @@
 class FibRange:
-    def __init__(self, start_of_range, end_of_range):
+    def __init__(self, start_of_range: int, end_of_range: int):
         self.__start_of_range = start_of_range
         self.__end_of_range = end_of_range
 
-    def fibonacci(self):
+    @property
+    def count_fibonacci(self):
         previous_num = 0
         start_count = 1
         fibbo_num = 0
+        fibbo_list = []
         while True:
             fibbo_num = previous_num + fibbo_num
 
             if self.__start_of_range < fibbo_num < self.__end_of_range:
-                print(fibbo_num)
+                fibbo_list.append(fibbo_num)
 
             previous_num = start_count
             start_count = fibbo_num
 
             if fibbo_num > self.__end_of_range:
-                break
+                return ', '.join(str(number) for number in fibbo_list)
 
 
-fib = FibRange(7, 35)
-fib.fibonacci()
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(description='This is the program that prints fibonacci numbers in definite range.')
+
+    parser.add_argument('start', type=int, default=0, help='Argument that defines start of counting fibonacci')
+    parser.add_argument('end', type=int, help='Argument that defines end of counting fibonacci')
+
+    try:
+        args = parser.parse_args()
+        fib = FibRange(args.start, args.end)
+        print(fib.count_fibonacci)
+    except:
+        parser.print_help()
